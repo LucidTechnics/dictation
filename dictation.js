fbf974b5e9908b2084b458f47fb55ba2eb134f88
Bediako George <bediako.george@lucidtechnics.com>
2012-08-22 19:53:48 -0400

    initial update of dictation

---
+++ dictation.js
@@ -0,0 +1,6 @@
+var util = require('util');
+var parser = require('./lib/parser');
+
+var resourceModel = parser.parse('An Offering\nhas a name which is required and has allowed values Jim, Rachel, Ben, Beth');
+
+console.log(util.inspect(resourceModel, false, 6));
