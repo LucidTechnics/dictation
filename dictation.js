f819e05a12a53d867e02ff06d2ea392b6100c718
Bediako George <bediako.george@lucidtechnics.com>
2012-09-02 11:18:56 -0400

    added more features

--- dictation.js
+++ dictation.js
@@ -1,6 +1,6 @@
 var util = require('util');
 var parser = require('./lib/parser');
 
-var resourceModel = parser.parse('An Offering\nhas a name which is required and has allowed values Jim, Rachel, Ben, Beth');
+var resourceModel = parser.parse('An Offering\nhas a name which is required and has allowed values Jim, Rachel, Ben, Beth and is encrypted');
 
 console.log(util.inspect(resourceModel, false, 6));
