14a05f785571e65b29839ede7a7d54a4f67e103b
Sina Viseh <sviseh1@umbc.edu>
2013-11-06 17:12:59 -0500

    remove unused files

--- dictation.js
+++
@@ -1,6 +0,0 @@
-var parser = require('./lib/parser');
-
-exports.parse = function(_dictation)
-{
-	return parser.parse(_dictation);
-};
--- test/testDictation.js
+++
@@ -1,6 +0,0 @@
-var util = require('util');
-var dictation = require('dictation');
-
-var resourceModel = dictation.parse('An Offering is abstract\nhas a name which is not required and has allowed values Jim, Rachel, Ben, Beth and is encrypted');
-
-console.log(util.inspect(resourceModel, false, 6));
